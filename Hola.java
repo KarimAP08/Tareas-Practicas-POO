a0bc21baa8530a71194831c2e55b0d53d01c3ae2
KarimAP08 <124391455+KarimAP08@users.noreply.github.com>
2023-02-03 12:23:26 -0600

    Add files via upload

---
+++ Hola.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hola;
+
+/**
+ *
+ * @author KarimAP
+ */
+public class Hola {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo! !");
+    }
+}

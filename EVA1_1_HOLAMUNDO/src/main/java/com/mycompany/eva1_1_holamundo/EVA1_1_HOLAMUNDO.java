e6e920fcfbd80df2cd608f221795f83c5e6c5d6a
KarimAP08 <124391455+KarimAP08@users.noreply.github.com>
2023-02-03 12:48:24 -0600

    Add files via upload

---
+++ EVA1_1_HOLAMUNDO/src/main/java/com/mycompany/eva1_1_holamundo/EVA1_1_HOLAMUNDO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_1_holamundo;
+
+/**
+ *
+ * @author KarimAP
+ */
+public class EVA1_1_HOLAMUNDO {
+
+    public static void main(String[] args) {
+        int x = 100;
+        System.out.println("Hola Mundo! !");
+        System.out.println(x);
+    }
+}

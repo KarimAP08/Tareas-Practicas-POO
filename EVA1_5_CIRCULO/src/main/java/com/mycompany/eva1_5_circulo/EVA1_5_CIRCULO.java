4aabe1c26bfd8c615ca94b7d1cd96b32fa4bfed4
KarimAP08 <124391455+KarimAP08@users.noreply.github.com>
2023-02-18 21:02:20 -0600

    Add files via upload

---
+++ EVA1_5_CIRCULO/src/main/java/com/mycompany/eva1_5_circulo/EVA1_5_CIRCULO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_5_circulo;
+
+/**
+ *
+ * @author KarimAP
+ */
+public class EVA1_5_CIRCULO {
+
+    public static void main(String[] args) {
+        Circulo Class1 = new Circulo(3, 1, 4);
+        Class1.setArea(3);
+        Class1.setPerimetro(1);
+        Class1.setRadio(4);
+        System.out.println(Class1.getArea());
+        System.out.println(Class1.getPerimetro());
+        System.out.println(Class1.getRadio()); 
+        //Class1.ImprimirDatos();
+    }
+}

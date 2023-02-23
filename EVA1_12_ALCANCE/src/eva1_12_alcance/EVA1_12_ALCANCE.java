d6a8392f9e7524e444af511fab3a99a1e4363749
KarimAP08 <124391455+KarimAP08@users.noreply.github.com>
2023-02-23 11:27:01 -0700

    Add files via upload

---
+++ EVA1_12_ALCANCE/src/eva1_12_alcance/EVA1_12_ALCANCE.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_12_alcance;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_12_ALCANCE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(x);//no existe, porque no esta declarada despues de la instruccion
+        System.out.println(args);//ars esta declarada antes de la instruccion
+        int x = 100;
+        System.out.println(x);
+    }
+    
+}
